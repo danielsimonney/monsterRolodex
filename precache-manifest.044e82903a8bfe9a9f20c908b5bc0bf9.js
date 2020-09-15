bae8780323ea90963b971ec713d16fd1931a6c77
danielsimonney <daniel.simonney60@gmail.com>
2020-09-15 10:59:00 +0200

    Updates

---
+++ precache-manifest.044e82903a8bfe9a9f20c908b5bc0bf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f13118266610bfc92f0363fe78a226b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7798346c48154721db5",
+    "url": "/static/css/main.50b1a266.chunk.css"
+  },
+  {
+    "revision": "de63e3d22144b6022c4b",
+    "url": "/static/js/2.5241892c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.5241892c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7798346c48154721db5",
+    "url": "/static/js/main.5c15e65e.chunk.js"
+  },
+  {
+    "revision": "299bd36d5458990039c2",
+    "url": "/static/js/runtime-main.5fa3ce95.js"
+  }
+]);
